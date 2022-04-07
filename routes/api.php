6ef9b19c1a8f9aa1310876a29089f60bd69fc043
Angel Pagola <angelpagolapajuelo99@gmail.com>
2022-04-06 21:38:03 -0500

    fix(usuario): Validación de datos de usuarios únicos  cuando se edita

--- routes/api.php
+++ routes/api.php
@@ -29,8 +29,8 @@ Route::prefix('/usuario')->controller(UsuarioController::class)->group(function 
     Route::get('/perfil/avatar/{id}', 'avatar')->name('usuario.avatar');
     Route::get('/perfil/interes/{id}', 'interes')->name('usuario.interes');
     Route::get('/perfil/{id}', 'show')->name('usuario.show');
-    Route::put('/perfil/update/{id}', 'usuarioUpdate')->name('usuario.update');
-    Route::put('/perfil/avatar/update/{id}', 'avatarUpdate')->name('usuario.avatar.update');
+    Route::put('/perfil/update/{usuario}', 'usuarioUpdate')->name('usuario.update');
+    Route::put('/perfil/avatar/update/{usuario}', 'avatarUpdate')->name('usuario.avatar.update');
     Route::delete('/perfil/interes/{usuario}/{tag}', 'interesDelete')->name('usuario.interes.delete');
 });
 
