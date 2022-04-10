3888bf153467bc2c16e2a4a1a8aac0bdc1406272
Frank Paucar <fcpaucar@gmail.com>
2022-04-09 22:49:36 -0500

    feat: se agregó la funcion para guardar un nuevo proyecto

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -6,11 +6,16 @@ use App\Http\Requests\storeProyecto;
 use App\Http\Requests\updateProyecto;
 use App\Models\Favorito;
 use App\Models\Proyecto;
+use App\Models\ProyectoArchivo;
+use App\Models\ProyectoImagen;
 use App\Models\ProyectoTag;
+use App\Models\Tag;
 use App\Models\Usuario;
 use App\Models\Valoracion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class ProyectoController extends Controller
@@ -56,15 +61,77 @@ class ProyectoController extends Controller
         return response()->json($proyecto, 200);
     }
 
-    public function store(storeProyecto $request)
+    public function store(Request $request)
     {
-        $proyecto = new Proyecto();
-        $proyecto->uuid = Str::uuid();
-        $proyecto->titulo = $request->titulo;
-        $proyecto->resumen = $request->resumen;
-        $proyecto->fecha_publicacion = $request->fecha_publicacion;
-        $proyecto->estudiante_id = $request->estudiante_id;
-        $proyecto->save();
+        $rules = [
+            "titulo" => "required|max:255",
+            "resumen" => "required",
+            'usuario_id' => "required|gt:0",
+            'tags' => 'required|array|min:2',
+            "imagenes" => "required",
+            "imagenes.*" => "image|mimes:png,jpeg,jpg",
+            "documentos" => "required",
+            "documentos.*" => "mimes:pdf,docx,xlsx",
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'respuesta' => false,
+                'mensaje' => 'Error de validación', 'error' => $validator->errors()
+            ], 200);
+        }
+
+        $usuario = Usuario::find($request->usuario_id);
+
+        $proyecto = Proyecto::create([
+            'uuid' => Str::uuid(),
+            'titulo' => $request->titulo,
+            'resumen' => $request->resumen,
+            'fecha_publicacion' => now()->format('Y-m-d'),
+            'estudiante_id' => $usuario->estudiante_id
+        ]);
+
+        $tags_id = [];
+        foreach ($request->tags as $tag) {
+            $tag_exists = Tag::query()->where('nombre', $tag)->first();
+            if (!$tag_exists) {
+                $tag_exists = Tag::create(['nombre' => $tag]);
+            }
+            $tags_id[] = $tag_exists->id;
+        }
+
+        $usuario->intereses()->attach($tags_id);
+        $proyecto->tags()->attach($tags_id);
+
+        $rutaImagenes = 'public/proyectos/imagenes';
+        $urlImagenes = 'storage/proyectos/imagenes/';
+        if (!Storage::exists($rutaImagenes)) {
+            Storage::makeDirectory($rutaImagenes);
+        }
+        $imagenes_ids = [];
+        foreach ($request->imagenes as $imagen) {
+            $nombreImagen = 'repotweb_' . time() . rand(1, 99) . '.' . $imagen->getClientOriginalExtension();
+            $link_imagen = asset($urlImagenes . $nombreImagen);
+            $imagen->storeAs($rutaImagenes, $nombreImagen);
+            $imagenes_ids[] = ["link_imagen" => $link_imagen, "proyecto_id" => $proyecto->id];
+        }
+        ProyectoImagen::insert($imagenes_ids);
+
+        $rutaDocumentos = 'public/proyectos/documentos';
+        $urlDocumentos = 'storage/proyectos/documentos/';
+        if (!Storage::exists($rutaDocumentos)) {
+            Storage::makeDirectory($rutaDocumentos);
+        }
+        $documentos_ids = [];
+        foreach ($request->documentos as $documento) {
+            $nombreDoc = 'repotweb_' . time() . rand(1, 99) . '.' . $documento->getClientOriginalExtension();
+            $link_doc = asset($urlDocumentos . $nombreDoc);
+            $documento->storeAs($rutaDocumentos, $nombreDoc);
+            $documentos_ids[] = ["link_archivo" => $link_doc, "proyecto_id" => $proyecto->id];
+        }
+        ProyectoArchivo::insert($documentos_ids);
 
         return response()->json([
             'respuesta' => true,
